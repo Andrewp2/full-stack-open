430cf603f264a0cc21cfe0a20ca03d18a149828f
andrewp2 <andrewp2@iastate.edu>
2020-10-11 14:22:50 -0700

    Finish part 1b

---
+++ part1/courseinfo/src/index.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+  return (
+    <h1>{props.course.name}</h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part = {props.course.parts[0]}/>
+      <Part part = {props.course.parts[1]}/>
+      <Part part = {props.course.parts[2]}/>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  )
+}
+
+const Total = (props) => {
+  const part_exercise_count = props.course.parts.map(part => part.exercises);
+  const sum = part_exercise_count.reduce((a, b) => a + b, 0);
+  return (
+    <p>Number of exercises {sum}</p>
+  )
+}
+
+
+
+const App = () => {
+  const course =  {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <>
+    <Header course={course}/>
+    <Content course={course}/>
+    <Total course={course}/>
+    </>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
